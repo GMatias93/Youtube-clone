bd63be19cd8482b61d43721b9ac84ccc902a7ef2
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-04 20:39:35 -0800

    feat: created SearchBar component with basic input funcitonality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyBh8wgrVhOlwl-_GTCtqt923IcxzjWaXdk';
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));

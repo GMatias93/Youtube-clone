075ad77078a161c677c537e6dd4aacf43fa6b43d
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-04 20:31:38 -0800

    feat: added youtube api key and youtube-api-search npm package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyBh8wgrVhOlwl-_GTCtqt923IcxzjWaXdk';
+
 const App = () => {
   return <div>Hello World!</div>;
 };

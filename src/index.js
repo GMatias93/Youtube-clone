292350e7895e521bd7c633d4ce5e934761eb4872
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-05 11:28:50 -0800

    feat: turned App into a class based component to make youtube API calls

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBh8wgrVhOlwl-_GTCtqt923IcxzjWaXdk';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'Drake' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));

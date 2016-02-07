0682d98d708e528a84951b3a2c7635050f91e233
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-02-07 12:55:56 -0800

    feat: added search functionality with searchBar

--- src/index.js
+++ src/index.js
@@ -14,7 +14,12 @@ class App extends Component {
       videos: [],
       selectedVideo: null,
     };
-    YTSearch({ key: API_KEY, term: 'Drake' }, (videos) => {
+
+    this.videoSearch('Drake');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0],
@@ -25,7 +30,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearch={term => this.  videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
         onVideoSelect={selectedVideo => {this.setState({ selectedVideo });}}
